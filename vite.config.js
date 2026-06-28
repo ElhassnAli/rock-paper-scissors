deea88d09aee4e3df25e18ba962dfd1974b65248
ElhassnAli <elassn57@gmail.com>
2026-06-29 02:21:30 +0300

    #

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-const REPO_NAME = "rock-paper-scissors";
 
 export default defineConfig({
-  base: "/rock-paper-scissors/",
   plugins: [react()],
 });
